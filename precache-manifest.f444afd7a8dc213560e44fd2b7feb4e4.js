939c6232f124bdcca52ce1d64ff0d1178eab4a34
shivam-github-coder <shivam.k.mzn@gmail.com>
2020-12-18 14:54:00 +0530

    Updates

---
+++ precache-manifest.f444afd7a8dc213560e44fd2b7feb4e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5e0dc08ed947f5db6ec91b0046b3416",
+    "url": "/netflix/index.html"
+  },
+  {
+    "revision": "59812b044c619628eb5a",
+    "url": "/netflix/static/css/main.313779d1.chunk.css"
+  },
+  {
+    "revision": "298055b787b6fed0cfa9",
+    "url": "/netflix/static/js/2.a7af9025.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/netflix/static/js/2.a7af9025.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59812b044c619628eb5a",
+    "url": "/netflix/static/js/main.40a546e1.chunk.js"
+  },
+  {
+    "revision": "0587df36741fdadb7899",
+    "url": "/netflix/static/js/runtime-main.5311aa9e.js"
+  },
+  {
+    "revision": "d25db23aa9a3cdcb3b64a302afab8681",
+    "url": "/netflix/static/media/netflix-logo1.d25db23a.png"
+  }
+]);
